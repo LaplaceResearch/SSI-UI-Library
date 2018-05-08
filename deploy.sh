d26a990837cf8d4d52e5bd4f7fae2a7111a93b04
ekinertac <ekinertac@gmail.com>
2018-05-08 23:11:54 +0300

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,21 @@ CDFOLDER="cd /home/deployer/cdn/$PROJECT"
 PULL="git pull"
 NGINX="sudo service nginx reload"
 
+ssh -t $CDN "$CDFOLDER && $PULL"
 
-if [ "$1" = "pack" ]; then
-    ssh -t $CDN "$CDFOLDER && $PULL"
-fi
+# if [ "$1" = "pack" ]; then
+#     ssh -t $CDN "$CDFOLDER && $PULL"
+# fi
 
-if [ "$1" = "nginx" ]; then
-    ssh -t $CDN "$CDFOLDER && $PULL && $NGINX"
-fi
+# if [ "$1" = "nginx" ]; then
+#     ssh -t $CDN "$CDFOLDER && $PULL && $NGINX"
+# fi
 
-if [ -z "$1" ]; then
-    echo
-    echo "Usage: "
-    echo
-    echo "./deploy.sh pack                      Deploy whole package."
-    echo "./deploy.sh nginx                     Deploy whole package and reload nginx."
-    echo
-fi
+# if [ -z "$1" ]; then
+#     echo
+#     echo "Usage: "
+#     echo
+#     echo "./deploy.sh pack                      Deploy whole package."
+#     echo "./deploy.sh nginx                     Deploy whole package and reload nginx."
+#     echo
+# fi
